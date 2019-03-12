dee70e3e491b2409c65288a2d7366b86428ff1a9
Jinny <jinny920311@gmail.com>
2019-03-12 21:29:34 +0900

    :pen: JavaScript 공부 - bingo 만들기 (html 세팅 및 li 가져오기) 작업을 위해 push

--- code/JavaScript/bingo/bingo.js
+++ code/JavaScript/bingo/bingo.js
@@ -1,20 +1,33 @@
 const bingoItem = document.getElementsByClassName('bingo__item');
 
 //select
-function selectItem() {
-   let li = this;
-    console.log(li);
+function selectItem(ddd) {
+   console.log('dddd', ddd);
+   this.classList.add('active');
+   let prev = this.previousElementSibling;
+   let next = this.nextElementSibling;
+   console.log(prev,next);
+   if(prev === 'null' || next === 'null'){
+
+   }
+   if(!!prev.classList.contains('active')){
+        console.log('이전꺼 클래스 있따');
+   }else {
+       console.log('없다')
+   }
+   let current = this.className.replace(/[^0-9]/g,'');
+   console.log(current);
+   switch (current) {
+       case '0': ;
+       break;
+   }
 }
 
 function getLi() {
     for (let i = 0; i < bingoItem.length; i ++) {
-        bingoItem[i].addEventListener('click',selectItem);
+        bingoItem[i].addEventListener('click',selectItem(i));
         console.log(bingoItem[i]);
     }
-    // for (let i in bingoItem) {
-    //     console.log(bingoItem[i]);
-    //     bingoItem[i].addEventListener('click',selectItem);
-    // }
 }
 
 function init() {
