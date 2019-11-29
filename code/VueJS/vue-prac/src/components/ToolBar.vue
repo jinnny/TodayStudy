aa504efaa9c29d6e8efc3dd6f4bc940073f0bac7
yangeunjin <yangej@wemakeprice.com>
2019-11-29 18:26:08 +0900

    vuejs 연습하기

---
+++ code/VueJS/vue-prac/src/components/ToolBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/news">News</router-link><br>
+    <router-link to="/ask">Ask</router-link><br>
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ToolBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
